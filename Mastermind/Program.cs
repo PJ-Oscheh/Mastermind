2d8f51232bb8528e8ab5ec4bcafb87273c100259
PJ-Oscheh <phillip236.po@gmail.com>
2025-08-23 14:26:51 -0400

    Add answer generator

--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Text;
 
 namespace Mastermind
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+
+            string answer = GenerateAnswer();
         }
+
+        /// <summary>
+        /// Generates a random answer.
+        ///
+        /// An answer consists of four digits ranging from 1-6, inclusive.
+        /// </summary>
+        /// <returns>The answer as a string</returns>
+        public static string GenerateAnswer()
+        {
+            Random rand = new();
+            StringBuilder tmpAnswer = new();
+
+            for (int i = 0; i < 4; i++)
+            {
+                tmpAnswer.Append(rand.Next(1, 7));
+            }
+            
+            return tmpAnswer.ToString();
+        }
+        
+        
     }
 }
