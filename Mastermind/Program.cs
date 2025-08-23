cbd76f2917bf27f6398a4221d7f541d40f21b55b
PJ-Oscheh <phillip236.po@gmail.com>
2025-08-23 14:19:41 -0400

    Initial commit, use older template style

---
+++ Mastermind/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mastermind
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
